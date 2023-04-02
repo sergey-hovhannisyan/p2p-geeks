770283073a112d9bcbc76fec1f73499f5a60214c
Sergey <thankfulcon@gmail.com>
2023-04-02 02:07:54 -0400

    basic blog page done

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import os
 
-parent_dir = os.path.dirname(os.getcwd())
+posts = [
+    {
+    "author" : "nic\k",
+    "title" : "Blog post 1",
+    "content" : "firt post content",
+    "date_posted" : "august 27"
+    },
+    {
+    "author" : "Jange",
+    "title" : "Blog post 2",
+    "content" : "second post content",
+    "date_posted" : "august 28"
+    }
+]
 
 def home(request):
-    return render(request, os.path.join(parent_dir, "frontend\index.html"))
+    context = {
+        "posts" : posts, 
+    }
+    return render(request, "blog/home.html", context)
+
+def about(request):
+    return render(request, "blog/about.html", {"title" : "about"})
