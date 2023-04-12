7d5ac83736620eb8c0c4bd6fe9ba3bd46cdaacc2
Sergey <thankfulcon@gmail.com>
2023-04-11 23:04:11 -0400

    user app modified

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
-# Create your views here.
+# def register(request):
+#     if request.method == "POST":
+#         form = UserRegistrationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("post-home")
+#     else:
+#         form = UserRegistrationForm()
+#     return render(request, "../frontend/register.html", {"form" : form})
+
+# def register(request):
+#     if request.method == "POST":
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("post-home")
+#     else:
+#         form = UserCreationForm()
+#     return render(request, "../frontend/register.html", {"form" : form})
+
+def register(request):
+    return render(request, "../frontend/register.html")
