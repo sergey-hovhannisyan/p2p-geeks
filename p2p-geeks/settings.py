b33b558d699f9917c552aa94c7a8b3d39961dfdf
Sergey <thankfulcon@gmail.com>
2023-04-23 20:05:13 -0400

    changed Debug to True
    need to change debug to false in runtime during demo

--- p2p-geeks/settings.py
+++ p2p-geeks/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-6i=l4df&kz01$(+d%0pqm*05-x6)^9vsu608dxdjf^9k26is%%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
